2d57812212b940fe18efff7053f228f2349ab5dc
dsjack <dsjack89@gmail.com>
2019-05-03 00:30:36 -0700

    Adds solution to Ch 12. Ex. 10

--- BJP4_12/src/main/java/Exercises.java
+++ BJP4_12/src/main/java/Exercises.java
@@ -2,15 +2,22 @@ import static java.lang.Integer.parseInt;
 
 public class Exercises {
     public static void main(String[] args) {
-        int i = 1234567;
-        int j = 67;
-        System.out.println(digitMatch(i, j));
+        // ***Ex 10.***
+        // int i = 1234567;
+        // int j = 67;
+        // System.out.println(digitMatch(i, j));
+
+        // ***Ex 11.***
+        // System.out.println(repeat("hi ho! ", 5));
+
+        // ***Ex 12.***
+        System.out.println(isReverse("CSE143", "341esc"));
 
     }
 
     public static int digitMatch(int d1, int d2) throws IllegalArgumentException {
         // Ch. 12 Ex. 10
-        
+
         // Write a recursive method digitMatch that accepts two non-negative
         // integers as parameters and that returns the number of digits that
         // match between them. Two digits match if they are equal and have the
@@ -52,6 +59,7 @@ public class Exercises {
             throw new IllegalArgumentException();
         }
 
+        // Base cases:
         if ((s1.length() == 1 && s2.length() == 1) && s1.charAt(0) == s2.charAt(0)) {
             // The integers are both one digit long AND are equal to each other
             return 1;
@@ -70,6 +78,7 @@ public class Exercises {
             return digitMatch(d1, Integer.parseInt(s2));
 
         } else {
+            // Recursive cases:
             int recursive = digitMatch(Integer.parseInt(s1.substring(0,
                     s1.length() - 1)), Integer.parseInt(s2.substring(0,
                     s2.length() - 1)));
@@ -81,6 +90,105 @@ public class Exercises {
             } else {
                 return recursive;
             }
+        }
+    }
+
+    public static String repeat(String s, int n) {
+        // Write a recursive method repeat that accepts a string s and an
+        // integer n as parameters and that returns a String consisting of n
+        // copies of s. For example:
+
+        // Call	                      Value Returned
+        // repeat("hello", 3)	      "hellohellohello"
+        // repeat("this is fun", 1)	  "this is fun"
+        // repeat("wow", 0)	          ""
+        // repeat("hi ho! ", 5)	      "hi ho! hi ho! hi ho! hi ho! hi ho! "
+
+        // You should solve this problem by concatenating String objects using
+        // the + operator. String concatenation is an expensive operation, so it
+        // is best to minimize the number of concatenation operations you
+        // perform. For example, for the call repeat("foo", 500), it would be
+        // inefficient to perform 500 different concatenation operations to
+        // obtain the result. Most of the credit will be awarded on the
+        // correctness of your solution independent of efficiency. The remaining
+        // credit will be awarded based on your ability to minimize the number
+        // of concatenation operations performed.
+
+        // Your method should throw an IllegalArgumentException if passed any
+        // negative value for n. You are not allowed to construct any structured
+        // objects other than Strings (no array, List, Scanner, etc.) and you
+        // may not use any loops to solve this problem; you must use recursion.
+
+        if (n < 0) {
+            throw new IllegalArgumentException("I can't go for that!");
+        }
+
+        if (n == 0) {
+            return "";
+        } else {
+            return s + repeat(s, n - 1);
+        }
+    }
+
+    public static boolean isReverse(String s1, String s2) {
+        // Ch 12. Ex 12
+
+        // Write a recursive method isReverse that accepts two strings as a
+        // parameter and returns true if the two strings contain the same
+        // sequence of characters as each other but in the opposite order
+        // (ignoring capitalization), and false otherwise. For example, the
+        // string "hello" backwards is "olleh", so a call of
+        // isReverse("hello", "olleh") would return true. Since the method is
+        // case-insensitive, you would also get a true result from a call of
+        // isReverse("Hello", "oLLEh"). The empty string, as well as any
+        // one-letter string, is considered to be its own reverse. The string
+        // could contain characters other than letters, such as numbers, spaces,
+        // or other punctuation; you should treat these like any other
+        // character. The key aspect is that the first string has the same
+        // sequence of characters as the second string, but in the opposite
+        // order, ignoring case. The table below shows more examples:
+
+
+        // Call	                            Value Returned
+        // isReverse("CSE143", "341esc")....true
+        // isReverse("Madam", "MaDAm")......true
+        // isReverse("Q", "Q")..............true
+        // isReverse("", "")................true
+        // isReverse("e via n", "N aIv E")..true
+        // isReverse("Go! Go", "OG !OG")....true
+        // isReverse("Obama", "McCain").....false
+        // isReverse("banana", "nanaba")....false
+        // isReverse("hello!!", "olleh")....false
+        // isReverse("", "x")...............false
+        // isReverse("madam I", "i m adam").false
+        // isReverse("ok", "oko")...........false
+
+        // You may assume that the strings passed are not null. You are not
+        // allowed to construct any structured objects other than Strings (no
+        // array, List, Scanner, etc.) and you may not use any loops to solve
+        // this problem; you must use recursion. If you like, you may declare
+        // other methods to help you solve this problem, subject to the previous
+        // rules.
+
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            // if string lengths don't match, false
+            return false;
+        }
+
+        if (s1.length() == 1 && !s1.equals(s2)) {
+            return false;
+        } else if (s1.length() == 1 || s1.equals("")) {
+            return true;
+        }
+
+        if (s1.charAt(0) == s2.charAt(s2.length() - 1)) {
+            return isReverse(s1.substring(1), s2.substring(0, s2.length() - 1));
+
+        } else {
+            return false;
         }
     }
 }
