5d290788ffdf6c8dfbfa234e5c2b69ae42df5d25
dsjack <dsjack89@gmail.com>
2019-12-05 13:54:16 -0800

    Adds isSorted() solution

--- BJP5_16/src/LinkedIntList.java
+++ BJP5_16/src/LinkedIntList.java
@@ -98,6 +98,23 @@ public class LinkedIntList {
     return current.data;
   }
 
+  public boolean isSorted() {
+    if (front == null) {
+      return true;
+    }
+    boolean result = true;
+    ListNode current = front;
+
+    while (current.next != null) {
+      if (current.data > current.next.data) {
+        result = false;
+        break;
+      }
+      current = current.next;
+    }
+    return result;
+  }
+
   // Removes and returns the first value
   // Throws a NoSuchElementException inn empty list
   public int remove() {
